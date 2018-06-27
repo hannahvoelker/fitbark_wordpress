3c8ed51a67975069545c3547a8d4cc828a1fca7b
Hannah Voelker <hannahvoelker13@gmail.com>
2018-06-26 20:31:39 -0400

    using a node package but don't get promises yet

--- plugin.js
+++ plugin.js
@@ -1,15 +1,65 @@
-require('http');
-var keygetter = require("./keys.js")
-
+var keygetter = require("./keys.js");
+var express = require('express')
+var app = express()
 var id = keygetter.get_id();
 var secret = keygetter.get_secret();
+var ClientOAuth2 = require('client-oauth2');
+
+var fitbarkAuth = new ClientOAuth2({
+  clientId: id,
+  clientSecret: secret,
+  accessTokenUri: "https://app.fitbark.com/oauth/token",
+  authorizationUri: "https://app.fitbark.com/oauth/authorize",
+  redirectUri: 'urn:ietf:wg:oauth:2.0:oob',
+  scopes: ['notifications', 'gist']
+});
+
+var token = fitbarkAuth.createToken('access token');
+// expires in a year
+token.expiresIn(365*24*60*60*60);
+token.refresh();
+
+storeNewToken = function(){
+	console.log("stored")
+}
+
+app.get('/auth/fitbark', function (req, res) {
+  var uri = fitbarkAuth.code.getUri()
+
+  res.redirect(uri)
+})
+
+// from the mulesoft oauth2 package
+app.get('/auth/fitbark/callback', function (req, res) {
+  fitbarkAuth.code.getToken(req.originalUrl)
+    .then(function (user) {
+      console.log(user) //=> { accessToken: '...', tokenType: 'bearer', ... }
+
+      // Refresh the current users access token.
+      user.refresh().then(function (updatedUser) {
+        console.log(updatedUser !== user) //=> true
+        console.log(updatedUser.accessToken)
+      })
+
+      // Sign API requests on behalf of the current user.
+      user.sign({
+        method: 'get',
+        url: 'http://example.com'
+      })
+
+      // We should store the token into a database.
+      return res.send(user.accessToken)
+    })
+})
 
 
-/* this needs to change once I figure out how to store the token on Raven
-perhaps a Mongo instance... tbd */
+
+/*  OLD CODE BELOW ~~ dunno if I am going to use the node pacakge or not
+this needs to change once I figure out how to store the token on Raven
+perhaps a Mongo instance... tbd 
 var authorized = false;
 
-/* authentication */ 
+// authentication 
 if(!authorized){
 	var request = new XMLHttpRequest();
 	request.open("POST", "https://app.fitbark.com/oauth/authorize", true);
@@ -38,4 +88,4 @@ requestToServer = function(access_code){
 		}
 	}
 	request.send();
-}
+} */
